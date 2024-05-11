c52a0e0a5b198ac2f63294c711d678fe02645b7d
ZinHt9tHlaing <yngWIE@9420>
2024-05-11 15:55:34 +0630

    update next

---
+++ mini_crud/src/app/create/page.tsx
@@ -0,0 +1,63 @@
+import { db } from "@/db/index";
+import { redirect } from "next/navigation";
+
+export default function Create() {
+  const createPost = async (formData: FormData) => {
+    "use server";
+
+    const title = formData.get("title") as string;
+    const description = formData.get("description") as string;
+
+    const post = await db.post.create({
+      data: {
+        title,
+        description,
+      },
+    });
+      console.log(post);
+      
+      redirect("/")
+  };
+
+  return (
+    <section className=" mt-40 w-1/2 mx-auto">
+      <h2 className=" text-center text-3xl font-bold uppercase">Create Page</h2>
+      <p className="text-center text-sm font-medium text-gray-400">
+        Create Your Own New Post now
+      </p>
+      <form className=" mt-6" action={createPost}>
+        <div className=" mb-4">
+          <label htmlFor="title" className=" text-lg font-medium text-gray-600">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className=" block border-2 border-gray-500 focus-visible:outline-none w-full p-2 active:scale-95 duration-700"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="description"
+            className=" text-lg font-medium text-gray-600"
+          >
+            Description
+          </label>
+          <textarea
+            name="description"
+            id="description"
+            rows={7}
+            className=" block border-2 border-gray-500 focus-visible:outline-none w-full p-2 active:scale-95 duration-700"
+          />
+        </div>
+        <button
+          type="submit"
+          className=" bg-black text-white w-full text-center py-4 mt-4 text-lg font-bold active:scale-95 duration-200"
+        >
+          Post
+        </button>
+      </form>
+    </section>
+  );
+}
