0c0aeaeb020e5a33fce8da6706d8d7560b89a4a6
ZinHt9tHlaing <yngWIE@9420>
2024-10-04 23:01:31 +0630

    provider setup

--- snap-shop/server/auth.ts
+++ snap-shop/server/auth.ts
@@ -1,10 +1,21 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import { db } from "@/server/index";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db) as any,
   session: { strategy: "jwt" },
   secret: process.env.AUTH_SECRET!,
-  providers: [],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
 });
